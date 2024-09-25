03eda331e4bf733960d3f0be917330dbd19370aa
smrenato <renatosmx@gmail.com>
2024-09-25 14:08:58 -0300

    fix: code refactored

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 
 def get_conf(conf_path: Path) -> dict:
-
     with open(conf_path, 'rb') as f:
         data = tomllib.load(f)
     return data
--- utils/data_sanity.py
+++ utils/data_sanity.py
@@ -1,46 +1,76 @@
-""" """
+"""
+Refactoring this shit code that was made
+"""
 
 from pathlib import Path
+from types import FunctionType
+from typing import Any
 
+import imagehash
 from PIL import Image, UnidentifiedImageError
 
+# Sanity check
 
-class SanityCheck:
-    def __init__(self, path: Path) -> None:
-        self.path = path
-        self.files_list = self.__get_path__list()
-        self.modes_types = []
-        self.width = []
-        self.height = []
 
-    def __get_path__list(self) -> list[Path]:
-        files_path_gen = self.path.glob('*.*')
-        return [file for file in files_path_gen]
+def get_path_list(path: Path) -> list[Path]:
+    """
+    @param
+    """
+    files_path_gen = path.glob('*.*')
+    return [file for file in files_path_gen]
 
-    def check_corruption(self) -> dict[str, int]:
-        error = []
-        for file in self.files_list:
-            try:
-                img = Image.open(file)
-                self.modes_types.append(img.mode)
-                self.width.append(img.width)
-                self.height.append(img.height)
 
-            except UnidentifiedImageError:
-                error.append(file)
-                print(f'error found on:{file}')
+def check_imgs(files_list: list[Path]) -> dict[str, Any]:
+    img_error = []
+    img_modes = []
+    img_width = []
+    img_height = []
+    img_format = []
 
-        return {'error': len(error), 'file_error': error}
+    for file in files_list:
+        try:
+            with Image.open(file) as img:
+                img_modes.append(img.mode)
+                img_width.append(img.width)
+                img_height.append(img.height)
+                img_format.append(img.format)
 
-    def check_typing(self) -> dict[str, list]:
-        ext = [str(file).split('.')[-1] for file in self.files_list]
-        return {'ext': list(set(ext))}
+        except UnidentifiedImageError:
+            img_error.append(file)
+    return {
+        'img_error': img_error,
+        'img_modes': img_modes,
+        'img_width': img_width,
+        'img_height': img_height,
+        'img_format': img_format,
+    }
 
-    def check_dimensions(self) -> dict[str, list]:
-        return {
-            'width': list(set(self.width)),
-            'height': list(set(self.height)),
-        }
 
-    def check_mode(self) -> dict[str, list]:
-        return {'modes': list(set(self.modes_types))}
+# No use
+def get_img_extensions(img_list: list[Path]) -> dict[str, list]:
+    ext = [str(file).split('.')[-1] for file in img_list]
+    ext = list(set(ext))
+
+    return {'extensions': ext}
+
+
+# REVIEW: maybe improve this with hamming approach, but not today!
+def find_similar_images(
+    path_img_list: Path, hash_func: FunctionType = imagehash.average_hash
+) -> dict[str, Path]:
+    """
+    Find similar images based on hash
+    """
+    hashed_img_dict = {}
+    for path_img in path_img_list:
+        try:
+            img = Image.open(path_img)
+            hashed_img = hash_func(img)
+            key = str(hashed_img)
+            # If the keys doesn't exist, create a new one and add a new list
+            hashed_img_dict[key] = hashed_img_dict.get(key, []) + [path_img]
+
+        except UnidentifiedImageError as e:
+            raise e
+
+        return hashed_img_dict
